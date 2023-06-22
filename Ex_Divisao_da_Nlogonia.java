3ac0ea132c64190bf017a620b6ddd28846342940
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-06-22 17:19:25 -0300

    Update Ex_Divisao_da_Nlogonia.java

--- Ex_Divisao_da_Nlogonia.java
+++ Ex_Divisao_da_Nlogonia.java
@@ -1,41 +1,47 @@
+//exercício "divisao de nlogonia" da prova, comentadinho.
 import java.util.Scanner;
 
-public class Ex_Divisao_da_Nlogonia{
+public class Ex_Divisao_da_Nlogonia {
     final static Scanner LER = new Scanner(System.in);
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        //declaração das variáveis.
         int i = 0;
         int k = 0;
         int n = 0;
         int m = 0;
         int x = 0;
         int y = 0;
-        String territorio = null;
+        String territorio = "";
 
+
+        //laço de repetição que continua executando indefinidamente até que a condição de saída seja atendida. No início de cada repetição, é chamado um método para ler "k". Se "k" for igual a zero, o loop é quebrado.
         while (true) {
             k = lerK(k);
             if (k == 0) {
                 break;
             }
 
+            //os valores de n e m são lidos por meio dos métodos que os leêm.
             n = lerN(n);
             m = lerM(m);
 
+            //um loop for é executado toda vez que "i" é menor que "k". A cada repetição, os valores de x e y são lidos por meio dos métodos que os leêm.
             for (i = 0; i < k; i++) {
-                if (k == 0) {
-                    break;
-                }
-
                 x = lerX(x);
                 y = lerY(y);
 
+                //método verificarTerritorio é chamado para determinar o território correspondente com base nos valores de x, y, n e m.
                 territorio = verificarTerritorio(x, y, n, m);
+
+                //método imprimirDivisao é chamado para mostrar o território destinado a cada valor inserido.
                 imprimirDivisao(territorio);
             }
         }
     }
 
-    public static int lerK(int k){
+    //método que lê o valor "k", mas permitindo somente a entrada de valores maiores que 0 e menores ou iguais a 1000.
+    public static int lerK(int k) {
         do {
             k = LER.nextInt();
         } while (k < 0 || k >= 1000);
@@ -43,41 +49,45 @@ public class Ex_Divisao_da_Nlogonia{
         return k;
     }
 
-    public static int lerN(int n){
+    //método que lê o valor "n", mas permitindo somente a entrada de valores maiores que -10000 e menores a 10000.
+    public static int lerN(int n) {
         do {
             n = LER.nextInt();
-        } while (n <= -10000 || n >= 10000);
+        } while (n < -10000 || n > 10000);
 
         return n;
     }
 
-    public static int lerM(int m){
+    //método que lê o valor "m", mas permitindo somente a entrada de valores maiores que -10000 e menores a 10000.
+    public static int lerM(int m) {
         do {
             m = LER.nextInt();
-        } while (m <= -10000 || m >= 10000);
+        } while (m < -10000 || m > 10000);
 
         return m;
     }
 
-    public static int lerX(int x){
+    //método que lê o valor "x", mas permitindo somente a entrada de valores maiores que -10000 e menores a 10000.
+    public static int lerX(int x) {
         do {
             x = LER.nextInt();
-        } while (x <= -10000 || x >= 10000);
+        } while (x < -10000 || x > 10000);
 
         return x;
     }
 
-    public static int lerY(int y){
+    //método que lê o valor "y", mas permitindo somente a entrada de valores maiores que -10000 e menores a 10000.
+    public static int lerY(int y) {
         do {
             y = LER.nextInt();
-        } while (y <= -10000 || y >= 10000);
+        } while (y < -10000 || y > 10000);
 
         return y;
     }
 
-    public static String verificarTerritorio(int x, int y, int n, int m){
-        
-        String territorio = null;
+    //método verificarTerritorio que determina o território correspondente com base nos valores de x, y, n e m. Se "x" for igual a "n" ou "y" for igual a "m" o território é a divisa e assim por diante.
+    public static String verificarTerritorio(int x, int y, int n, int m) {
+        String territorio = "";
 
         if (x == n || y == m) {
             territorio = "divisa";
@@ -94,7 +104,8 @@ public class Ex_Divisao_da_Nlogonia{
         return territorio;
     }
 
-    public static void imprimirDivisao(String territorio){
+    //método que imprime o território destinado a cada valor inserido.
+    public static void imprimirDivisao(String territorio) {
         System.out.println(territorio);
     }
 }
